5133724e11822e6c811005cdfca80635335bc6ee
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 03:19:06 +0100

    typo

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_list(mxd_lst: List[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Calculate the sum of a list of integers and floats."""
     result: float = 0.0
     for i in input_list:
