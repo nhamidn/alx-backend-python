0b42c8673e397b240075bdfe90a504013856943b
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 03:56:43 +0100

    101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""safely_get_value Module"""
+from typing import Mapping, TypeVar, Any, Union
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)\
+        -> Union[Any, T]:
+    """safely_get_value function."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
