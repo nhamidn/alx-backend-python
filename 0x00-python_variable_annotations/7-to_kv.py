d3dcdab866eebe99e2c40e9b51a3afd22428d5f3
nhamidn <hamid-savana@hotmail.fr>
2023-12-08 03:26:45 +0100

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """to_kv Module"""
-from typing import List, Union
+from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Create a tuple with the string k and the square of the int/float v."""
+    """Create a tuple with the k and the square of v."""
     return str(k), float(v ** 2)
