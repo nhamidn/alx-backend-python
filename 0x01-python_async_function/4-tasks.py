ec3a49643f96816fd928ea2a02b24aa2b203d1be
nhamidn <hamid-savana@hotmail.fr>
2023-12-12 04:09:18 +0100

    4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+import random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n function."""
+    delays = []
+    tasks = []
+
+    for i in range(n):
+        task = task_wait_random(max_delay)
+        task.add_done_callback(lambda x: delays.append(x.result()))
+        tasks.append(task)
+
+    for task in tasks:
+        await task
+    return delays
