4a3d8caf89a7bdd1721be9fdaa905b61350e023d
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-08 20:08:01 +0000

    test_get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Dict, Tuple, Union
 
 
@@ -28,13 +28,29 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), KeyError),
     ])
     def test_access_nested_map_exception(
-        self, n_map: Dict, path: Tuple[str], exception: Union[KeyError]
+            self, n_map: Dict, path: Tuple[str], exception: Union[KeyError]
             ) -> None:
         """
         method to test that access_nested_map returns the correct exception.
         """
         with self.assertRaises(exception):
             access_nested_map(n_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests that tests the get_json emthod in utils."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self, test_url: str, test_payload: Dict,
+            ) -> None:
+        """Method to Test that get_json returns the correct json."""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
 
 """if __name__ == '__main__':
