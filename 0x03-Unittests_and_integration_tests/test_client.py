423349103b0f554813443818d7886b07e12ef987
Noureddine hamid <hamid-savana@hotmail.fr>
2024-04-12 22:55:05 +0000

    test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -15,17 +15,15 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"}),
     ])
-    @patch('client.get_json')
-    def test_org(
-            self, org: str, resp: Dict, mocked_fn: Mock
-            ) -> None:
-        """
-        method to test that GithubOrgClient.org returns the correct value.
-        """
-        mocked_fn.return_value = MagicMock(return_value=resp)
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        """Function that tests GithubOrgClient.org."""
+        mocked_fxn.return_value = MagicMock(return_value=resp)
         gh_org_client = GithubOrgClient(org)
         self.assertEqual(gh_org_client.org(), resp)
-        mocked_fn.assert_called_once_with(
+        mocked_fxn.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
 
